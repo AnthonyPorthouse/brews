0f5da5aa11ba2045706212d7b24631e357ef2f26
Anthony Porthouse <anthony.porthouse@groundsix.com>
2016-09-13 11:36:14 +0100

    Use gsed instead of unix sed

--- ocrdownloader.rb
+++ ocrdownloader.rb
@@ -5,8 +5,12 @@ class Ocrdownloader < Formula
   sha256 "188974c300617fa6b29b99312d0ce2443da8ff83bfcdff6164d3e48addbfe82a"
 
   depends_on "aria2"
+  depends_on "gnu-sed"
 
   def install
+    inreplace "ocrdownloader" do |s|
+      s.gsub! /sed -r/, "gsed -r"
+    end
     bin.install "ocrdownloader"
   end
 
