afe0d70c9ea574d7261134abc9627eeaf816a976
Anthony Porthouse <anthony.porthouse@groundsix.com>
2016-09-13 11:20:07 +0100

    Added OCR Downloader

---
+++ ocrdownloader.rb
@@ -0,0 +1,16 @@
+class Ocrdownloader < Formula
+  desc "Terminal Based downloader for OC ReMix."
+  homepage "https://github.com/AnthonyPorthouse/ocrdownloader"
+  url "https://github.com/AnthonyPorthouse/ocrdownloader/archive/v1.0.0.tar.gz"
+  sha256 "188974c300617fa6b29b99312d0ce2443da8ff83bfcdff6164d3e48addbfe82a"
+
+  depends_on "aria2"
+
+  def install
+    bin.install "ocrdownloader"
+  end
+
+  test do
+    system "which", "ocrdownloader"
+  end
+end
