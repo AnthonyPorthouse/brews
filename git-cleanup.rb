79c9f06c868a304fbf408487bf16945d03f873ac
Anthony Porthouse <anthony.porthouse@opia-sp.com>
2017-11-24 10:48:16 +0000

    Added git-cleanup formula

---
+++ git-cleanup.rb
@@ -0,0 +1,16 @@
+class GitCleanup < Formula
+  desc "Auto remove merged branches from your local repository"
+  homepage "https://github.com/AnthonyPorthouse/git-cleanup"
+  url "https://github.com/AnthonyPorthouse/git-cleanup/archive/v0.1.1.tar.gz"
+  sha256 "00a0bb81cb7e4b60fba9892dc7e4c465c0923b66fa8e0a6ce7099b9d3a2665ff"
+
+  depends_on "git"
+
+  def install
+    bin.install "git-cleanup"
+  end
+
+  test do
+    system "which", "git-cleanup"
+  end
+end
