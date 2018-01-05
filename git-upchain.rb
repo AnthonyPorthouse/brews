db51199e233d2eae60b1014b3bb9238c5964a979
Anthony Porthouse <anthony.porthouse@opia-sp.com>
2018-01-05 11:58:07 +0000

    improve formula

--- git-upchain.rb
+++ git-upchain.rb
@@ -4,13 +4,11 @@ class GitUpchain < Formula
   url "https://github.com/AnthonyPorthouse/git-upchain/archive/v0.0.1.tar.gz"
   sha256 "d9cd7a1738113f3d928aaddec608f88cac267c9390d313efc7c3a50050428e32"
 
-  depends_on "git"
-
   def install
     bin.install "git-upchain"
   end
 
   test do
-    system "#{bin}/git-upchain --version"
+    system "#{bin}/git-upchain", "--version"
   end
 end
