650eefbe3bdef2f1a7b185d751eea7f5e4d8eaf7
Anthony Porthouse <anthony.porthouse@opia-sp.com>
2018-01-05 11:54:13 +0000

    Updated git-upchain homepage

--- git-upchain.rb
+++ git-upchain.rb
@@ -1,6 +1,6 @@
 class GitUpchain < Formula
   desc "Create merges from branch to branch easily"
-  homepage ""
+  homepage "https://github.com/AnthonyPorthouse/git-upchain"
   url "https://github.com/AnthonyPorthouse/git-upchain/archive/v0.0.1.tar.gz"
   sha256 "d9cd7a1738113f3d928aaddec608f88cac267c9390d313efc7c3a50050428e32"
 
