49b628fd8a24f5943b585cdb4518e8b5bab3c552
Anthony Porthouse <anthony@porthou.se>
2018-02-07 17:30:09 +0000

    Update git-upchain version

--- git-upchain.rb
+++ git-upchain.rb
@@ -1,8 +1,8 @@
 class GitUpchain < Formula
   desc "Create merges from branch to branch easily"
   homepage "https://github.com/AnthonyPorthouse/git-upchain"
-  url "https://github.com/AnthonyPorthouse/git-upchain/archive/v0.0.1.tar.gz"
-  sha256 "d9cd7a1738113f3d928aaddec608f88cac267c9390d313efc7c3a50050428e32"
+  url "https://github.com/AnthonyPorthouse/git-upchain/archive/v0.0.2.tar.gz"
+  sha256 "65aead964edbe98314b6adeff11b4424ca37a246d71e1301dc9c107a08349bad"
 
   def install
     bin.install "git-upchain"
