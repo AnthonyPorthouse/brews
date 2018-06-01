51480b5c5fe875f5b0ce7563e8c4dd662f7eff17
Anthony Porthouse <anthony.porthouse@opia-sp.com>
2018-06-01 09:44:53 +0100

    Add git-pr

---
+++ git-pr.rb
@@ -0,0 +1,14 @@
+class GitPr < Formula
+  desc "Create GitHub PRs from your current branch"
+  homepage "https://github.com/AnthonyPorthouse/git-pr"
+  url "https://github.com/AnthonyPorthouse/git-upchain/archive/v0.0.1.tar.gz"
+  sha256 "0ffdd690df993c243f41106fafe6555533d101b144a3a3d25bac719dc34b6615"
+
+  def install
+    bin.install "git-pr"
+  end
+
+  test do
+    system "#{bin}/git-pr", "--version"
+  end
+end
