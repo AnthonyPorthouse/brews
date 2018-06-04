bb7e3c5155224044e59b155e4f407f9cd29ca10a
Anthony Porthouse <anthony.porthouse@opia-sp.com>
2018-06-04 14:36:48 +0100

    Fix git-pr download url

--- git-pr.rb
+++ git-pr.rb
@@ -1,8 +1,9 @@
 class GitPr < Formula
   desc "Create GitHub PRs from your current branch"
   homepage "https://github.com/AnthonyPorthouse/git-pr"
-  url "https://github.com/AnthonyPorthouse/git-upchain/archive/v0.0.1.tar.gz"
+  url "https://github.com/AnthonyPorthouse/git-pr/archive/v0.0.1.tar.gz"
   sha256 "0ffdd690df993c243f41106fafe6555533d101b144a3a3d25bac719dc34b6615"
+  revision 1
 
   def install
     bin.install "git-pr"
