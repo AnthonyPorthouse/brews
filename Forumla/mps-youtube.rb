a287bd481517cc77b291004684ac96826232c39f
Anthony Porthouse <anthony.porthouse@opia-sp.com>
2018-06-14 13:19:59 +0100

    Update Structure

--- Forumla/mps-youtube.rb
+++ Forumla/mps-youtube.rb
@@ -1,17 +1,17 @@
 class MpsYoutube < Formula
   desc "Terminal-based YouTube player and downloader."
   homepage "https://github.com/np1/mps-youtube"
-  url "https://github.com/mps-youtube/mps-youtube/archive/v0.2.6.tar.gz"
-  sha256 "df3cc7ad9a49a0da390c38f40c905daa992d48de5fb421865ec19cc1fbec453f"
+  url "https://github.com/mps-youtube/mps-youtube/archive/v0.2.8.tar.gz"
+  sha256 "d5f2c4bc1f57f0566242c4a0a721a5ceaa6d6d407f9d6dd29009a714a0abec74"
 
-  depends_on :python3
+  depends_on "python"
   depends_on "youtube-dl"
   depends_on "mpv"
   depends_on "mplayer"
 
   resource "pafy" do
-    url "https://github.com/mps-youtube/pafy/archive/develop.tar.gz"
-    # sha256 "eda55b14ec4ecfa5eb6c9834fc295c5885d93d5cfbf6c2452d1b7ffff1e59351"
+    url "https://github.com/mps-youtube/pafy/archive/v0.5.4.tar.gz"
+    sha256 "a2f9fe7c6175264993190081e1558516b2f2b5987f13a581a55cd9d838df973b"
   end
 
   def install
